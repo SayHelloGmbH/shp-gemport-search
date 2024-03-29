34c7a6d5954e7951cfe2d49d91a76b67f5d011d3
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-03-29 09:47:28 +0100

    SImplify check logic

--- src/Blocks/GemportSearch/render.php
+++ src/Blocks/GemportSearch/render.php
@@ -7,7 +7,7 @@ $generation = (int) $attributes['generation'] ?? '';
 $postcode = $attributes['postcode'] ?? '';
 
 // check to see that $postcode is a four-digit numeric between 1000 and 9999
-if (!is_numeric($postcode) || strlen($postcode) !== 4 || $postcode < 1000 || $postcode > 9999) {
+if (!is_numeric($postcode) || strlen($postcode) !== 4) {
 	$postcode = '';
 }
 
