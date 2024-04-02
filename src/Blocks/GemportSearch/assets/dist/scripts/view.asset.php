e9e1a02ab99bdf06a800c26b63adf558edad8c3d
Mark Howells-Mead <mark@sayhello.ch>
2024-04-02 17:46:01 +0200

    Scroll position

--- src/Blocks/GemportSearch/assets/dist/scripts/view.asset.php
+++ src/Blocks/GemportSearch/assets/dist/scripts/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-i18n'), 'version' => 'a63630306567b7a6ed70');
+<?php return array('dependencies' => array('wp-element', 'wp-i18n'), 'version' => '619bf50d8993f24b331d');
--- src/Blocks/GemportSearch/assets/src/scripts/view/index.js
+++ src/Blocks/GemportSearch/assets/src/scripts/view/index.js
@@ -1,4 +1,4 @@
-import { render, useMemo, useState } from '@wordpress/element';
+import { render, useEffect, useMemo, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 import { AppContext } from './_context';
@@ -10,9 +10,10 @@ const App = ({ element }) => {
 
 	const [dataPostcode] = useState(postcode || '');
 	const [dataSearch, setDataSearch] = useState('');
+	const [initialRender, setInitialRender] = useState(true);
+	const [listData, setListData] = useState([]);
 	const [selectedThemes, setSelectedThemes] = useState([]);
 	const [viewMode, setViewMode] = useState('form');
-	const [listData, setListData] = useState([]);
 	const themesEndpoint = useMemo(() => `https://gemport.ch/gemport/public/api/themes?generation=${generation}`, []);
 	const selectedThemesString = selectedThemes.join(',');
 
@@ -29,6 +30,38 @@ const App = ({ element }) => {
 
 		return url;
 	}, [dataPostcode, selectedThemesString, dataSearch]);
+
+	// On component mount
+	useEffect(() => {
+		setInitialRender(false);
+	}, []);
+
+	// Scroll to the top of the element which switching modes
+	useEffect(() => {
+		if (!element || initialRender) {
+			return;
+		}
+
+		const { top } = element.getBoundingClientRect();
+
+		const cssValue = getComputedStyle(document.body).getPropertyValue('--gemport-search--scroll-offset', 0);
+		const regex = /(\d+)px/g; // Regular expression to match numerical values followed by "px"
+		const matches = cssValue.match(regex);
+
+		let result = parseInt(cssValue, 10);
+		if (matches) {
+			result = matches.reduce((acc, val) => acc + parseInt(val, 10), 0);
+		}
+
+		const scrollOffset = parseInt(result, 10);
+
+		let scrollToY = top + window.scrollY - scrollOffset;
+
+		window.scrollTo({
+			top: Math.max(0, scrollToY),
+			behavior: 'smooth',
+		});
+	}, [viewMode, element, initialRender]);
 
 	const { data, error, state } = apiGet(themesEndpoint);
 
