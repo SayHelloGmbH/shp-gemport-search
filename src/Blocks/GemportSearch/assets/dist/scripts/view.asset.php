c40a3be846b94f6cd1fb92ad663bb9b34e1f708c
Mark Howells-Mead <mark@sayhello.ch>
2024-04-04 15:34:32 +0200

    Handle invalid town response from API

--- src/Blocks/GemportSearch/assets/dist/scripts/view.asset.php
+++ src/Blocks/GemportSearch/assets/dist/scripts/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-i18n'), 'version' => 'e691abaf9abca64b4759');
+<?php return array('dependencies' => array('wp-element', 'wp-i18n'), 'version' => '72eac3395a35cec0846b');
